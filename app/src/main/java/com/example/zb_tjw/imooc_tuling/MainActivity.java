6c0bc41655b2562324056886d578eb9361b7649f
zb_tjw_mac <1549790231@qq.com>
2019-01-11 16:17:06 +0800

    第一次提交初建项目

---
+++ app/src/main/java/com/example/zb_tjw/imooc_tuling/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zb_tjw.imooc_tuling;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
