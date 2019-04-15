7ad3a97fe0ca233cd6606bedad2a96b1577f4dff
zb_tjw_win <1549790231@qq.com>
2019-04-15 22:04:12 +0800

    1.修改了测试类的代码，测试类里的result在HttpUtils里解析好了，所以在解析会报错，格式出问题，已修正

--- app/src/main/java/com/example/zb_tjw/imooc_tuling/test/TestHttpUtils.java
+++ app/src/main/java/com/example/zb_tjw/imooc_tuling/test/TestHttpUtils.java
@@ -21,12 +21,13 @@ public class TestHttpUtils{
         String result = null;
         try {
             result = HttpUtils.post("{\"reqType\":0,\"perception\":{\"inputText\":{\"text\":\"给我讲个笑话\"}},\"userInfo\":{\"apiKey\":\"b45dde26f2e843848d8f2c1aba7cf74d\",\"userId\":\"378152\"}}");
-            JSONObject jsonObject = JSONObject.parseObject(result);//结果转化为JSONObject对象
-            JSONArray jsonArray = jsonObject.getJSONArray("results");//直接从这个JSONObject获取到results数组
-            JSONObject jsonObject1 = jsonArray.getJSONObject(0);//再获取results数组中所有对象
-            JSONObject jsonObject2 = jsonObject1.getJSONObject("values");//获取到values对应的值
-            String resultText = jsonObject2.getString("text");//获取最后有用的文本数据
-            System.out.println("resultText==="+resultText);
+//            JSONObject jsonObject = JSONObject.parseObject(result);//结果转化为JSONObject对象
+//            JSONArray jsonArray = jsonObject.getJSONArray("results");//直接从这个JSONObject获取到results数组
+//            JSONObject jsonObject1 = jsonArray.getJSONObject(0);//再获取results数组中所有对象
+//            JSONObject jsonObject2 = jsonObject1.getJSONObject("values");//获取到values对应的值
+//            String resultText = jsonObject2.getString("text");//获取最后有用的文本数据
+//            System.out.println("resultText==="+resultText);
+            System.out.println("resultText==="+result);
         } catch (IOException e) {
             e.printStackTrace();
         }
